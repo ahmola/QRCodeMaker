46d5ccb3ce9060c9dcf2111e5d66b9636647517e
Jason <66013853+ahmola@users.noreply.github.com>
2024-02-24 15:34:38 +0900

    try to add oauth2 but I think I have to try at other project lol

---
+++ src/main/java/dev/practice/QRCodeGenerator/controller/web/Oauth2Controller.java
@@ -0,0 +1,10 @@
+package dev.practice.QRCodeGenerator.controller.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class Oauth2Controller {
+}
